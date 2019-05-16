d3c6d5a7a2e3cd0eab439a5b59c4768f00671d0c
Pontus Ellstrom <pontus.ellstrom@forsakringskassan.se>
2019-05-16 18:34:55 +0200

    tom tabell

--- client/src/components/SamlingsTabell.vue
+++ client/src/components/SamlingsTabell.vue
@@ -1,16 +1,8 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-    <v-data-table
-            :headers="headers"
-            :items="desserts"
-            class="elevation-1"
-    >
+    <v-data-table :headers="headers" :items="Person" class="elevation-1">
         <template v-slot:items="props">
             <td>{{ props.item.name }}</td>
-            <td class="text-xs-right">{{ props.item.calories }}</td>
-            <td class="text-xs-right">{{ props.item.fat }}</td>
-            <td class="text-xs-right">{{ props.item.carbs }}</td>
-            <td class="text-xs-right">{{ props.item.protein }}</td>
-            <td class="text-xs-right">{{ props.item.iron }}</td>
+            <td class="text-xs-right">{{ props.item.namn }}</td>
         </template>
     </v-data-table>
 </template>
@@ -18,130 +10,54 @@
 
 <script>
     export default {
-        data () {
+        data() {
             return {
                 headers: [
-                    {
-                        text: 'Dessert (100g serving)',
-                        align: 'left',
-                        sortable: false,
-                        value: 'name'
-                    },
-                    { text: 'Calories', value: 'calories' },
-                    { text: 'Fat (g)', value: 'fat' },
-                    { text: 'Carbs (g)', value: 'carbs' },
-                    { text: 'Protein (g)', value: 'protein' },
-                    { text: 'Iron (%)', value: 'iron' }
+                    {text: 'Namn', value: 'name'},
+                    {text: '1', value: 'Day1'},
+                    {text: '2', value: 'Day2'},
+                    {text: '3', value: 'Day3'},
+                    {text: '4', value: 'Day4'},
+                    {text: '6', value: 'Day5'},
+                    {text: '7', value: 'Day6'},
+                    {text: '8', value: 'Day7'},
+                    {text: '9', value: 'Day8'},
+                    {text: '10', value: 'Day9'},
+                    {text: '11', value: 'Day10'},
+                    {text: '12', value: 'Day11'},
+                    {text: '13', value: 'Day12'},
+                    {text: '15', value: 'Day13'},
+                    {text: '16', value: 'Day14'},
+                    {text: '17', value: 'Day15'},
+                    {text: '18', value: 'Day16'},
+                    {text: '19', value: 'Day17'},
+                    {text: '20', value: 'Day20'},
+                    {text: '21', value: 'Day21'},
+                    {text: '22', value: 'Day22'},
+                    {text: '23', value: 'Day23'},
+                    {text: '24', value: 'Day24'},
+                    {text: '25', value: 'Day25'},
+                    {text: '26', value: 'Day26'},
+                    {text: '27', value: 'Day27'},
+                    {text: '28', value: 'Day28'},
+                    {text: '29', value: 'Day29'},
+                    {text: '30', value: 'Day30'},
+                    {text: '31', value: 'Day31'},
+
                 ],
-                desserts: [
+                Person: [
                     {
-                        name: 'Frozen Yogurt',
-                        calories: 159,
-                        fat: 6.0,
-                        carbs: 24,
-                        protein: 4.0,
-                        iron: '1%'
-                    },
-                    {
-                        name: 'Ice cream sandwich',
-                        calories: 237,
-                        fat: 9.0,
-                        carbs: 37,
-                        protein: 4.3,
-                        iron: '1%'
-                    },
-                    {
-                        name: 'Eclair',
-                        calories: 262,
-                        fat: 16.0,
-                        carbs: 23,
-                        protein: 6.0,
-                        iron: '7%'
-                    },
-                    {
-                        name: 'Cupcake',
-                        calories: 305,
-                        fat: 3.7,
-                        carbs: 67,
-                        protein: 4.3,
-                        iron: '8%'
-                    },
-                    {
-                        name: 'Gingerbread',
-                        calories: 356,
-                        fat: 16.0,
-                        carbs: 49,
-                        protein: 3.9,
-                        iron: '16%'
-                    },
-                    {
-                        name: 'Jelly bean',
-                        calories: 375,
-                        fat: 0.0,
-                        carbs: 94,
-                        protein: 0.0,
-                        iron: '0%'
-                    },
-                    {
-                        name: 'Lollipop',
-                        calories: 392,
-                        fat: 0.2,
-                        carbs: 98,
-                        protein: 0,
-                        iron: '2%'
-                    },
-                    {
-                        name: 'Honeycomb',
-                        calories: 408,
-                        fat: 3.2,
-                        carbs: 87,
-                        protein: 6.5,
-                        iron: '45%'
-                    },
-                    {
-                        name: 'Donut',
-                        calories: 452,
-                        fat: 25.0,
-                        carbs: 51,
-                        protein: 4.9,
-                        iron: '22%'
-                    },
-                    {
-                        name: 'KitKat',
-                        calories: 518,
-                        fat: 26.0,
-                        carbs: 65,
-                        protein: 7,
-                        iron: '6%'
+                        namn: 'Test Testorsson',
+                        kortid: '88880001',
+                        dagar: [
+                            null,
+                            null,
+                            "Flexledig",
+                            null
+                            ]
                     }
                 ]
             }
         }
     }
 </script>
-<!--<script>
-    export default {
-        data() {
-            return {
-                personer: [
-                    { namn: "Pontus", day1: 1, day2: 2, day3: 3, last_name: 'Macdonald' },
-                    { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-                    { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-                    { age: 38, first_name: 'Jami', last_name: 'Carney' }
-                ],
-                numberOfDays: ''
-            }
-        },
-        methods: {
-            date: function() {
-                var now = new Date();
-                var lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
-                return lastDay.getDate();
-            }
-        },
-        created() {
-            this.numberOfDays = this.date()
-        }
-    }
-
-</script>-->
