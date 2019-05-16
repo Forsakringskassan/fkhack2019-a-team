d8562305e8cfca74fcad54035f62e58bd34db72f
Mikael Ivarsson <mikael.ivarsson@fk.se>
2019-05-16 19:08:04 +0200

    hämta ledighetstyper

--- client/src/components/AnsokanCard.vue
+++ client/src/components/AnsokanCard.vue
@@ -3,32 +3,30 @@
 
         <div class="card">
             <div class="card card-body">
-                <h1>ANSÖKAN</h1>
+                <h1>Ledighet</h1>
                 <div class="card-deck">
 
                     <div class="card">
                         <div class="card-header">From</div>
                         <div class="card-body">
-                            <datepicker :value="date.today"></datepicker>
+                            <datepicker v-model="date.from"></datepicker>
                         </div>
 
                     </div>
                     <div class="card">
                         <div class="card-header">Tom</div>
                         <div class="card-body">
-                            <datepicker :value="date.tomorrow"></datepicker>
+                            <datepicker v-model="date.tom"></datepicker>
                         </div>
 
                     </div>
 
                     <div class="card">
-                        <div class="card-header">Typ</div>
+                        <div class="card-header">Typ {{selected}}</div>
                         <div class="card-body">
                             <div class="form-group">
-                                <select class="form-control">
-                                    <option>Semester</option>
-                                    <option>Flex</option>
-                                    <option>Föräldraledighet</option>
+                                <select class="form-control" v-model="selected">
+                                    <option v-for="typ in ledighetstyper" :key="typ">{{typ}}</option>
                                 </select>
                             </div>
                         </div>
@@ -45,6 +43,8 @@
 
 <script>
     import datepicker from 'vuejs-datepicker'
+    import moment from 'moment';
+    import axios from 'axios';
 
     export default {
         name: "AnsokanCard",
@@ -52,9 +52,57 @@
         data() {
             return {
                 date: {
-                    today: new Date(),
-                    tomorrow: new Date()
-                }
+                    from: moment().toDate(),
+                    tom: moment(moment()).add(1, 'days').toDate()
+                },
+                selected: 'Semester',
+                ledighetstyper: []
+
+
+            }
+        },
+        computed: {
+            dates: function () {
+                let from = moment().toDate();
+                let tom = moment(from).add(1, 'days').toDate();
+
+                return {from: from, tom: tom};
+            }
+        },
+        created() {
+          this.getLedighetstyper();
+        },
+        methods: {
+            ansok: function () {
+                let url = 'http://localhost:8080/mft/user/ledighet/create';
+
+                let body = {
+                    kortid: this.$store.getters.getUser.kortid,
+                    from: this.formatDate(this.date.from),
+                    tom: this.formatDate(this.date.tom),
+                    ledighetstyp: this.selected
+                };
+
+                console.log(JSON.stringify(body, null, 1));
+                axios.post(url, body).then(function (response) {
+
+                    console.log(response);
+                })
+            },
+            getLedighetstyper: function() {
+
+                let self = this;
+
+                axios.post('http://localhost:8080/mft/ledighetstyper', {headers: {'charset': 'utf8'}}).then(function (response) {
+                    self.ledighetstyper = response.data;
+                })
+            },
+            formatDate: function (date) {
+
+
+                let month = date.getMonth() <= 9 ? "0"+(date.getMonth()+1) : (date.getMonth() +1);
+
+                return date.getFullYear() + '-'+ month + '-' + date.getDate();
 
             }
         }
