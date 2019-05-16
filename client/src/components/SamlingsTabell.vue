d2d3dbc6f1f15958aea98d3ff0c022a909341d91
Pontus Ellstrom <pontus.ellstrom@forsakringskassan.se>
2019-05-16 21:15:59 +0200

    lägger dynamiskt in tabell headers

--- client/src/components/SamlingsTabell.vue
+++ client/src/components/SamlingsTabell.vue
@@ -1,109 +1,58 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-<div>
     <v-data-table :headers="headers" :items="Personer" class="elevation-1">
         <template v-slot:items="props">
             <td>{{ props.item.namn }}</td>
         </template>
     </v-data-table>
-</div>
 </template>
 
 
 <script>
     import axios from 'axios';
+
     export default {
         data() {
             return {
-                antalDagar : '',
-                headers: [
-                    {text: 'Namn', value: 'name'},
-                    {text: '1', value: 'Day1'},
-                    {text: '2', value: 'Day2'},
-                    {text: '3', value: 'Day3'},
-                    {text: '4', value: 'Day4'},
-                    {text: '6', value: 'Day5'},
-                    {text: '7', value: 'Day6'},
-                    {text: '8', value: 'Day7'},
-                    {text: '9', value: 'Day8'},
-                    {text: '10', value: 'Day9'},
-                    {text: '11', value: 'Day10'},
-                    {text: '12', value: 'Day11'},
-                    {text: '13', value: 'Day12'},
-                    {text: '15', value: 'Day13'},
-                    {text: '16', value: 'Day14'},
-                    {text: '17', value: 'Day15'},
-                    {text: '18', value: 'Day16'},
-                    {text: '19', value: 'Day17'},
-                    {text: '20', value: 'Day20'},
-                    {text: '21', value: 'Day21'},
-                    {text: '22', value: 'Day22'},
-                    {text: '23', value: 'Day23'},
-                    {text: '24', value: 'Day24'},
-                    {text: '25', value: 'Day25'},
-                    {text: '26', value: 'Day26'},
-                    {text: '27', value: 'Day27'},
-                    {text: '28', value: 'Day28'},
-                    {text: '29', value: 'Day29'},
-                    {text: '30', value: 'Day30'},
-                    {text: '31', value: 'Day31'},
-
-                ],
+                headers: [],
                 Personer: [],
-                /*Person: [
-                    {
-                        namn: 'Test Testorsson',
-                        kortid: '88880001',
-                        dagar: [
-                            '1',
-                            '2',
-                            '3',
-                            '4',
-                            '6',
-                            '7',
-                            '8',
-                            '9',
-                            '10',
-                            '11',
-                            '12',
-                            '13',
-                            '15',
-                            '16',
-                            '17',
-                            '18',
-                            '19',
-                            '20',
-                            '21',
-                            '22',
-                            '23',
-                            '24',
-                            '25',
-                            '26',
-                            '27',
-                            '28',
-                            '29',
-                            '30',
-                            '31',
-                            ]
-                    }
-                ]*/
             }
         },
         methods: {
-            getPersoner(){
+            getPersoner() {
                 let self = this;
-                axios.post("http://localhost:8080/mft/enhetsvy", {kortid: this.$store.getters.getUser}).then(function (response) {
+                let date = new Date()
+                let month = date.getMonth() <= 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+                let firstDay = date.getFullYear() + '-' + month + '-' + date.getDate();
+
+                axios.post("http://localhost:8080/mft/enhetsvy", {
+                    kortid: this.$store.getters.getUser,
+                    date: firstDay
+                }).then(function (response) {
                     self.Personer = response.data
                 });
             },
 
-            daysInCurrentMonth () {
-                var datum = new Date();
-                this.antalDagar = new Date(datum.getFullYear(),datum.getMonth()+1, 0).getDate();
+            daysInCurrentMonth(month) {
+                let datum = new Date();
+                let antal = new Date(datum.getFullYear(), month + 1, 0).getDate();
+                let dagar = new Object();
+                let array = []
+                dagar.text = "Namn";
+                dagar.value = "name";
+                array.push(dagar);
+                this.headers = array;
+
+                for (var i = 1; i <= antal; i++) {
+                    let dagar = new Object();
+                    dagar.text = [i]
+                    dagar.value = [i]
+                    array.push(dagar)
+                }
             }
         },
         created() {
             this.getPersoner()
-            this.daysInCurrentMonth()
+            this.daysInCurrentMonth(new Date().getMonth())
         }
     }
 </script>
