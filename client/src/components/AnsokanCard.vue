0476a12393cccdc57a7a9599f1ff0b35093dd1d7
Mikael Ivarsson <mikael.ivarsson@fk.se>
2019-05-16 16:58:53 +0200

    ansökan

--- client/src/components/AnsokanCard.vue
+++ client/src/components/AnsokanCard.vue
@@ -3,7 +3,40 @@
 
         <div class="card">
             <div class="card card-body">
+                <h1>ANSÖKAN</h1>
+                <div class="card-deck">
 
+                    <div class="card">
+                        <div class="card-header">From</div>
+                        <div class="card-body">
+                            <datepicker :value="date.today"></datepicker>
+                        </div>
+
+                    </div>
+                    <div class="card">
+                        <div class="card-header">Tom</div>
+                        <div class="card-body">
+                            <datepicker :value="date.tomorrow"></datepicker>
+                        </div>
+
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">Typ</div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <select class="form-control">
+                                    <option>Semester</option>
+                                    <option>Flex</option>
+                                    <option>Föräldraledighet</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" @click="ansok" type="button">Skicka</button>
             </div>
         </div>
 
@@ -11,8 +44,20 @@
 </template>
 
 <script>
+    import datepicker from 'vuejs-datepicker'
+
     export default {
-        name: "AnsokanCard"
+        name: "AnsokanCard",
+        components: {datepicker},
+        data() {
+            return {
+                date: {
+                    today: new Date(),
+                    tomorrow: new Date()
+                }
+
+            }
+        }
     }
 </script>
 
